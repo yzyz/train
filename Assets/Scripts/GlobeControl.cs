cbb0abcec2ffeb51c92e427284bd312e21dec959
James Lin <james97lin@gmail.com>
2017-03-05 13:51:13 -0800

    Added overworld scene with corresponding scripts

---
+++ Assets/Scripts/GlobeControl.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobeControl : MonoBehaviour {
+
+	public enum GlobeState {
+		IDLE, MOVING, DRAGGING, STOPPED
+	}
+
+	/* Objects */
+	public GameObject globe;
+
+	/* Constants */
+	public float distFromCam = 1;
+	public float waitTime = 2f;
+	public float velocityDeltaTime = .1f;
+	public float angleDecay = .8f;
+	public float tolerance = .1f;
+	public float maxSpeed = 1;
+	public float acceleration = .05f;
+
+	/* Objects and Components */
+	private GlobeState globeState;
+	private GlobeControl spinScript;
+	private GameObject activeController;
+
+	/* Logic / Function Variables */
+	private Quaternion originalRotation;
+	private Quaternion movementRotation;
+	private float movementDeltaTime;
+	private Vector3 startVec;
+	private LinkedList<Quaternion> rots;
+	private LinkedList<float> times;
+	private float angleScale = 1;
+	private float storedTime;
+	private float currSpeed;
+
+	// Use this for initialization
+	void Start () {
+		currSpeed = maxSpeed;
+
+		globeState = GlobeState.IDLE;
+
+		rots = new LinkedList<Quaternion> ();
+		times = new LinkedList<float> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// FixedUpdate is called once every fixed time frame
+	void FixedUpdate () {
+		print (globeState);
+		if (globeState == GlobeState.DRAGGING) {
+			SteamVR_TrackedObject trackedObj = activeController.GetComponent<SteamVR_TrackedObject> ();
+			SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)trackedObj.index);
+			Vector3 currPos = trackedObj.transform.position;
+
+			if (device.GetPress (SteamVR_Controller.ButtonMask.Trigger)) {
+				// Update Rotation
+				Vector3 endVec = (currPos - globe.transform.position);
+				Quaternion rot = Quaternion.FromToRotation (startVec, endVec);
+				globe.transform.rotation = rot * originalRotation;
+
+				// Update lists
+				rots.AddLast (globe.transform.rotation);
+				times.AddLast (Time.time);
+				while (times.First.Value < Time.time - velocityDeltaTime) {
+					rots.RemoveFirst ();
+					times.RemoveFirst ();
+				}
+			} else if (device.GetPressUp (SteamVR_Controller.ButtonMask.Trigger)) {
+				// Set / Reinitialize values
+				movementRotation = Quaternion.Inverse (rots.First.Value) * globe.transform.rotation;
+				movementDeltaTime = Time.time - times.First.Value;
+				angleScale = 1;
+				globeState = GlobeState.MOVING;
+			}
+		} else if (globeState == GlobeState.MOVING) {
+			// Check if we've stopped
+			if (angleScale <= tolerance) {
+				globeState = GlobeState.STOPPED;
+				storedTime = Time.time;
+			} else { // Otherwise update globe rotation
+				globe.transform.rotation *= Quaternion.Slerp (Quaternion.identity, movementRotation, Time.fixedDeltaTime / movementDeltaTime * angleScale);
+				angleScale *= angleDecay;
+			}
+		} else if (globeState == GlobeState.STOPPED) {
+			if (Time.time - storedTime > waitTime) {
+				globeState = GlobeState.IDLE;
+			}
+		} else if (globeState == GlobeState.IDLE) {
+			// Rotate by velocity
+			transform.Rotate (new Vector3 (0, currSpeed, 0));
+
+			// Accelerate speed to a cap
+			currSpeed += acceleration;
+			if (currSpeed > maxSpeed) {
+				currSpeed = maxSpeed;
+			}
+		}
+			
+	}
+
+	public void OnTriggerStay(Collider other) {
+		// TODO: put in check that it's a controller that triggered it
+
+		SteamVR_TrackedObject trackedObj = other.gameObject.GetComponent<SteamVR_TrackedObject> ();
+		SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)trackedObj.index);
+
+		// We're in range and we've hit the trigger
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			// Grab our position in world space
+			Vector3 currPos = other.transform.position;
+
+			// Reinitialize values
+			originalRotation = globe.transform.rotation;
+			startVec = (currPos - globe.transform.position);
+			rots.Clear ();
+			times.Clear ();
+
+			// Set variables
+			activeController = other.gameObject;
+			globeState = GlobeState.DRAGGING;
+
+		}
+	}
+}
---
+++ Assets/Scripts/MouseControl.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour {
+
+	public enum GlobeState {
+		IDLE, MOVING, DRAGGING, STOPPED
+	}
+
+	/* Objects */
+	public GameObject globe;
+
+	/* Constants */
+	public float distFromCam = 1;
+	public float waitTime = 2f;
+	public float velocityDeltaTime = .1f;
+	public float angleDecay = .8f;
+	public float tolerance = .1f;
+
+	/* Objects and Components */
+	private Camera cam;
+	private GlobeState globeState;
+	private GlobeControl spinScript;
+
+	/* Logic / Function Variables */
+	private Quaternion originalRotation;
+	private Quaternion movementRotation;
+	private float movementDeltaTime;
+	private Vector3 startVec;
+	private LinkedList<Quaternion> rots;
+	private LinkedList<float> times;
+	private float angleScale = 1;
+	private float storedTime;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+		globeState = GlobeState.IDLE;
+		spinScript = globe.GetComponent<GlobeControl>();
+
+		rots = new LinkedList<Quaternion> ();
+		times = new LinkedList<float> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		/* INPUT (replace with VR version when necessary) */
+		// Set the current position from the mouse
+		Vector3 currPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distFromCam));
+
+		// Handle input
+		if (Input.GetMouseButtonDown (0)) {
+
+			// Disable idling rotation
+			//spinScript.StopSpin ();
+
+			// Reinitialize values
+			originalRotation = globe.transform.rotation;
+			startVec = (currPos - globe.transform.position);
+			rots.Clear ();
+			times.Clear ();
+		}
+
+		if (Input.GetMouseButton (0)) {
+			// Update Rotation
+			Vector3 endVec = (currPos - globe.transform.position);
+			Quaternion rot = Quaternion.FromToRotation (startVec, endVec);
+			globe.transform.rotation = rot * originalRotation;
+
+			// Update lists
+			rots.AddLast(globe.transform.rotation);
+			times.AddLast (Time.time);
+			while (times.First.Value < Time.time - velocityDeltaTime) {
+				rots.RemoveFirst ();
+				times.RemoveFirst ();
+			}
+
+		} else if (Input.GetMouseButtonUp (0)) {
+			// Set / Reinitialize values
+			movementRotation = Quaternion.Inverse(rots.First.Value) * globe.transform.rotation;
+			movementDeltaTime = Time.time - times.First.Value;
+			angleScale = 1;
+			globeState = GlobeState.MOVING;
+		}
+
+		// Handle movement from previous momentum
+		if (globeState == GlobeState.MOVING) {
+
+			// Check if we've stopped
+			if (angleScale <= tolerance) {
+				globeState = GlobeState.STOPPED;
+				storedTime = Time.time;
+			}
+
+			// Update globe rotation
+			globe.transform.rotation *= Quaternion.Slerp (Quaternion.identity, movementRotation, Time.fixedDeltaTime / movementDeltaTime * angleScale);
+			angleScale *= angleDecay;
+
+		} else if (globeState == GlobeState.STOPPED) {
+			if (Time.time - storedTime > waitTime) {
+				globeState = GlobeState.IDLE;
+				//spinScript.StartSpin ();
+			}
+		}
+	}
+}
