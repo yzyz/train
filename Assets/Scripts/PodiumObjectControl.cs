1fdcae94b1a0ddc8c4b4f32238ff6cea64e8ae25
James Lin <james97lin@gmail.com>
2017-04-18 16:08:14 -0700

    Finished up untested, basic overworld. There are podiums with floating objects on them, you hover and click on one to go to the corresponding level.

---
+++ Assets/Scripts/PodiumObjectControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PodiumObjectControl : MonoBehaviour {
+
+	public string sceneName;
+	public Vector3 rot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rot);
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (!other.tag.Equals("GameController")) return;
+
+		SteamVR_TrackedObject trackedObj = other.GetComponentInParent<SteamVR_TrackedObject>();
+		SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+
+		if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
