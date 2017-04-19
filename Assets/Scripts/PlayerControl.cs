bfb005f34b2333e5a8630c4e63de422d14d2dfec
James Lin <james97lin@gmail.com>
2017-04-19 01:44:10 -0700

    Changes to snakes in preparation for testing

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour {
+
+	public string overworld = "Overworld";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Fade to black and send the player back to the Overworld
+	void Die () {
+		//TODO: fade to black
+
+		SceneManager.LoadScene (overworld);
+	}
+
+	// Flash red
+	void Injure () {
+
+	}
+}
